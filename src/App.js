c83c3a09fccec8a9e50159c90cfbee36364463d2
Elliott Johnson <elliotthkj1991@gmail.com>
2023-05-17 21:39:27 -0700

    comics in context assignment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import './App.css';
-
-import UserInterface from "./UserInterface"
+import React from "react";
+import "./App.css";
+import StyleContext from "./StyleContext";
+import UserInterface from "./UserInterface";
 
 function App() {
+  const styling = {
+    background: "#1ECD97",
+    display: "inline-block",
+    width: 120,
+    height: 55,
+    fontSize: 18,
+    letterSpacing: 1,
+    border: "2px solid #1ECD97",
+    borderRadius: 15,
+  };
+
+  const headerStyle = {
+    color: "#FFFFFF",
+    fontFamily: "Luminari, fastasy",
+    fontSize: 20,
+  };
+
   return (
     <main>
-     <h1
+      <StyleContext.Provider value={styling}>
+        <h1
           style={{
             color: "#FEE001",
             fontFamily: "Luminari, fantasy",
-            fontSize: 50
+            fontSize: 50,
           }}
         >
           Welcome to Comics Galore!
         </h1>
         <UserInterface />
+      </StyleContext.Provider>
     </main>
   );
 }
--- src/UserInterface.jsx
+++ src/UserInterface.jsx
@@ -1,16 +1,22 @@
-import React from "react"
-import CollectorButton from './CollectorButton'
-import SubmitForm from './SubmitForm'
+import React, { useContext } from "react";
+import CollectorButton from "./CollectorButton";
+import SubmitForm from "./SubmitForm";
+import StyleContext from "./StyleContext";
 
-export default function UserInterface () {
+export default function UserInterface() {
+  const { headerStyle } = useContext(StyleContext);
+
+  
   return (
     <div>
-      <h2>We carry the latest and greatest editions of your favorite comic books!</h2>
+      <h2>
+        We carry the latest and greatest editions of your favorite comic books!
+      </h2>
       <h2>Are you a collector?</h2>
       <CollectorButton />
 
       <h2>Have a special request? Click here to fill out our form</h2>
       <SubmitForm />
     </div>
-  )
+  );
 }
